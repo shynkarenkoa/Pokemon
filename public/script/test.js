dcfcf706055b65174b35ec128b470027d17c28af
shynkarenkoa <shynkarenko.js@gmail.com>
2016-04-08 00:14:50 +0300

    in pokemon.less set style for html & write functional in test.js (fetch with setData, select with getting info and changing list)

--- public/script/test.js
+++ public/script/test.js
@@ -1,41 +1,57 @@
 'use strict';
 
-// fetch('http://pokeapi.co/api/v1/pokedex/1/').then(function(response) {  
-//     console.log(response.headers.get('Content-Type'));  
-//     console.log(response.headers.get('Date'));
-//     console.log(response.status);  
-//     console.log(response.statusText);  
-//     console.log(response.type);  
-//     console.log(response.url);  
-// });
+var pokemonsData = {}
+var links = document.querySelectorAll("span");
+var images = document.querySelectorAll(".list img");
+var part = 0;
 
 
-// fetch('http://pokeapi.co/api/v1/pokedex/1/')  
-//   .then(  
-//     function(response) 
-//     {  
-//       if (response.status !== 200) 
-//       {  
-//         console.log('Looks like there was a problem. Status Code: ' +  
-//           response.status);  
-//         return;  
-//       }
+getData(part);
 
-//         response.json().then
-//         (function(data) {console.log(data)} )  
-//     }  
-//   )  
-//   .catch(function(err) {  
-//     console.log('Fetch Error :-S', err);  
-//   });
+function getData(offset)
+{
+	fetch(`http://pokeapi.co/api/v1/pokemon/?offset=${offset}&limit=12`)  
+  .then(  
+    function(response) 
+    {  
+      if (response.status !== 200) 
+      {  
+        console.log('Looks like there was a problem. Status Code: ' +  
+          response.status);  
+        return;  
+      }
+        response.json().then(function(data)
+        {
+        	for (var i = 0; i < 12; i++)
+        	{
+        		images[i].src = `http://pokeapi.co/media/img/${data.objects[i].pkdx_id}.png`
+ 				links[i].setAttribute("id",data.objects[i].pkdx_id);
+        	}
+        	pokemonsData = data;
+        	console.log(pokemonsData) ///testing response data
+        } )
+    }  
+  )  
+  .catch(function(err) {  
+    console.log('Fetch Error : ', err);  
+  });
+}
 
 
-var images = document.querySelectorAll("img");
-//var Images = document.getElementsByTagName("img");
-console.log(images)
+function select(id)
+{
+	console.log(id.getAttribute("id"))
+	document.querySelector("sidebar").style.visibility="visible";
+	document.querySelector("#pic").src = `http://pokeapi.co/media/img/${id.getAttribute("id")}.png`
+	
+}
+
+function change()
+{
+	part +=12;
+	getData(part);
+	document.querySelector("sidebar").style.visibility="hidden";
+}
 
 
-for (var i = 0; i < images.length; i++)
- images[i].src = `http://pokeapi.co/media/img/${i+1}.png`
-
-	 
+	
